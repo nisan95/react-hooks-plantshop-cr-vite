cef312c648f03b0b6dd7c00f18019671e09c2ad7
sanora nigtherson <sanoranigtherson95@gmail.com>
2025-07-18 17:23:24 -0700

    ajout

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, searchTerm }) {
+  const visiblePlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {visiblePlants.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} />
+      ))}
+    </ul>
   );
 }
+
 
 export default PlantList;
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleAddPlant(newPlant) {
+    setPlants(prevPlants => [...prevPlants, newPlant]);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data))
+      .catch((error) => {
+      console.error("Error fetching plants:", error)});
+
+  }, []);
+
+  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={plants} searchTerm={searchTerm} />
     </main>
   );
 }
-
 export default PlantPage;
